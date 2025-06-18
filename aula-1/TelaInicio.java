ec37f46052b642247fbc8d6983120e63f2a66e72
marcelo <marcelocarneirodev@gmail.com>
2025-06-18 10:09:44 -0300

    adiciona classe TelaInicio com método main para exibir mensagens de boas-vindas e instruções

---
+++ aula-1/TelaInicio.java
@@ -0,0 +1,9 @@
+class TelaInicio{
+		
+	public static void main(String[] args){
+		System.out.println("-----------Bem vindo-----------");
+		System.out.println("-----------Insira seu Cartão:-----------");
+		System.out.println("-----------Insira sua Senha:-----------");
+	}
+
+}
