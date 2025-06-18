ef2ef8de09e28f70ba5ced5edaacec7e89365587
marcelo <marcelocarneirodev@gmail.com>
2025-06-18 10:09:23 -0300

    adiciona classe CartaoVisita com método main para exibir informações pessoais

---
+++ aula-1/CartaoVisita.java
@@ -0,0 +1,11 @@
+class CartaoVisita{
+	public static void main (String[] args){
+
+		System.out.println("********Cartão de Visita********");
+		System.out.println("Nome: João Marcelo Carneiro da silva");
+		System.out.println("Curso: Analise e Desenvolvimento de Sistemas");
+		System.out.println("Hobby: Tocar Violão");
+		System.out.println("*********************************");
+
+	}
+}
