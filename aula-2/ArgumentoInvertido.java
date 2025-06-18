eaf28d6c14c8b35b3128302d160bdcc2705b2f15
marcelo <marcelocarneirodev@gmail.com>
2025-06-18 11:10:11 -0300

    adiciona classe ArgumentoInvertido com método main para exibir informações a partir de argumentos

---
+++ aula-2/ArgumentoInvertido.java
@@ -0,0 +1,7 @@
+class ArgumentoInvertido{
+	public static void main (String[] args){
+	System.out.println("seu nome é:" + args[2]);
+	System.out.println("seu bairro é:" + args[1]);
+	System.out.println("gosta de:" + args[0]);
+	}
+}
